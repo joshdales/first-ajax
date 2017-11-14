0081fd6f3de44d370fab20ee9ab50b996265ff6b
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-14 15:33:54 -0500

    Append response to the step3456 section

--- ajax.js
+++ ajax.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   var firstButton = document.getElementById('first-button');
+  var step3456 = document.getElementById('step3456');
   var secondButton = document.getElementById('second-button');
 
   firstButton.addEventListener('click', function() {
@@ -14,6 +15,8 @@ document.addEventListener("DOMContentLoaded", function() {
     $.ajax({
       url: 'http://first-ajax-api.herokuapp.com/ping',
       method: 'GET',
+    }).done(function(data) {
+      step3456.appendChild(document.createElement('p')).innerText = data;
     })
   })
 
