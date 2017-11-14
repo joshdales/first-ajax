e9520ae8c150b348c51faf19ee0e676cfa583677
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-14 16:05:43 -0500

    Get time from request

--- ajax.js
+++ ajax.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function() {
   var secondButton = document.getElementById('second-button');
   var step7 = document.getElementById('step7')
   var thirdButton = document.getElementById('third-button');
+  var forthButton = document.getElementById('forth-button');
+  var step8 = document.getElementById('step8');
 
   firstButton.addEventListener('click', function() {
     $.ajax({
@@ -27,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log('There was an error that prevented the request from being made successfully');
       step3456.appendChild(document.createElement('p')).innerText = "I'm very sorry but something has gone wrong";
     })
-  })
+  });
 
   thirdButton.addEventListener('click', function() {
     $.ajax({
@@ -37,6 +39,15 @@ document.addEventListener("DOMContentLoaded", function() {
     }).done(function(data) {
       step7.appendChild(document.createElement('p')).innerText = data;
     })
+  });
+
+  forthButton.addEventListener('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      data: 'text'
+    })
+
   })
 
 
